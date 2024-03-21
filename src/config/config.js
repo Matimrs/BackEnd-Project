a441b39d477f9c0d2b9a7786eb15bd56ec50fc6f
Matias Morales <mati.morales.tepp@gmail.com>
2024-03-21 18:33:45 -0300

    Implementacion de variable de entorno segun el entorno de trabajo

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+let PORT, API_SESSION;
+
+if (process.env.ENV_MODE === "development") {
+  PORT = process.development.env.PORT;
+  API_SESSION = process.development.env.API_SESSION;
+} else {
+  PORT = process.production.env.PORT;
+  API_SESSION = process.production.env.API_SESSION;
+}
+
 export default {
   tokenSecret: process.env.TOKEN_SECRET,
   cookieToken: process.env.COOKIE_TOKEN,
   connectMongo: process.env.CONNECT_MONGO,
-  port: process.env.PORT,
+  port: PORT,
   clientIDGitHub: process.env.CLIENT_ID_GITHUB,
   clientSecretGitHub: process.env.CLIENT_SECRET_GITHUB,
   callbackURLGitHub: process.env.CALLBACK_URL_GITHUB,
   lastNameGitHub: process.env.GITHUB_LAST_NAME,
   passwordGitHub: process.env.GITHUB_PASSWORD,
-  sessionAPI: process.env.API_SESSION,
+  sessionAPI: API_SESSION,
 };
