942c68660a80867aab4a9d3704fe2cce49923ced
Matias Morales <mati.morales.tepp@gmail.com>
2024-05-10 00:35:34 -0300

    Implementacion de product test completo

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -4,13 +4,67 @@ import { app } from "../app.js";
 
 describe("Products router", () => {
   describe("GET /", () => {
-    it("", (done) => {
+    it("Deberia devolver un conjunto de productos", (done) => {
       request(app)
         .get("/api/products")
         .expect(200)
         .end((err, res) => {
           if (err) return done(err);
-          expect(res.body).to.be.an("array");
+          done();
+        });
+    });
+  });
+
+  describe("GET /:pid", () => {
+    it("Deberia devolver un producto especifico", (done) => {
+      request(app)
+        .get("/api/products/65a9b776ca2ffb8c669be621") 
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  describe("POST /", () => {
+    it("Deberia agregar un nuevo producto", (done) => {
+      request(app)
+        .post("/api/products")
+        .send({ title: "Auto 5",
+        description: "Automovil categoria 5",
+        code: "aaammmmer",
+        price: 3400000,
+        stock: 5,
+        category: "Vehiculo" })
+        .expect(201)
+        .end((err, res) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  describe("PUT /:pid", () => {
+    it("Deberia actualizar un producto especifico", (done) => {
+      request(app)
+        .put("/api/products/65a9b75cca2ffb8c669be61b") 
+        .send({ title:  "moto 3"})
+        .expect(201)
+        .end((err, res) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  describe("DELETE /:pid", () => {
+    it("Deberia eliminar un producto especifico", (done) => {
+      request(app)
+        .delete("/api/products/65a8b13088f6efb942593df8")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
           done();
         });
     });
